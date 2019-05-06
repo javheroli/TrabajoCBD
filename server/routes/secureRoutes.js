7833282958d94f09af543f889aa2df6d64b1a1b3
Unknown <javierherraizolivas@gmail.com>
2019-05-06 19:15:52 +0200

    [Back-end]: Find all users route

--- server/routes/secureRoutes.js
+++ server/routes/secureRoutes.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const Message = require('../models/messageModel');
+const User = require('../models/userModel');
 
+//API Route /api/chat/users/
+//GET: Getting all responses from DB
+//POST: Creating a new response and storing it at DB
+router.route('/users')
+    .get((req, res) => {
+        User.find({}, (err, users) => {
+            res.json(users)
+            console.log("Getting all users");
+            res.end();
+        })
+    })
 
 
 
