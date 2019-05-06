18a6ff6bd5d8d2497deb92ab63b05c8015aac5f4
javheroli <javierherraizolivas@gmail.com>
2019-05-06 16:15:02 +0200

    Project schema with backend added

---
+++ server/routes/routes.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+//Route  /api/user/signup
+//Authenticate user based on the data sended by user previously
+router.post('/signup', passport.authenticate('signup', {
+  session: false
+}), async (req, res, next) => {
+  res.json({
+    message: 'Signup successful',
+    user: req.user
+  });
+});
+
+//Route /api/user/login
+//Creation of login token for user
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('login', async (err, user, info) => {
+    try {
+      if (err || !user) {
+        return next(res.status(401).send(
+          'An Error occured: ' + info['message']
+        ));
+      }
+      req.login(user, {
+        session: false
+      }, async (error) => {
+        if (error) return next(error)
+
+        //We include in the token the following data: id and username
+        const body = {
+          _id: user._id,
+          username: user.username
+        };
+        //Create the token by signing in JWT
+        const token = jwt.sign({
+          user: body
+        }, 'v3Ry_DiFfiCuLT_k3Y');
+        //Send back the token to the user
+        return res.json({
+          token,
+          "message": info['message'],
+        });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+});
+
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const passport = require('passport');
+var routes = require('./routes/routes.js');
+var secureRoutes = require('./routes/secureRoutes.js');
+var bodyParser = require('body-parser');
+require('./auth/auth');
+
+
+//Connection to DataBase:
+//To connect to Development environment DB (Comment line below if not using it)
+mongoose.connect('mongodb://localhost:27017/TrabajoCBD', {
+  useNewUrlParser: true
+});
+
+//To connect to DB in Mlab
+//mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+
+mongoose.connection.on('error', error => console.log(error));
+mongoose.Promise = global.Promise;
+
+
+const app = express();
+
+//Defines server responses format as JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//Allowing Cross-Origin Request
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, OPTIONS, PUT, DELETE');
+  //intercepts OPTIONS method
+  if ('OPTIONS' === req.method) {
+    //respond with 200
+    res.sendStatus(200);
+  } else {
+    //move on
+    next();
+  }
+});
+
+// Serve static files from the Ionic app
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Server path to user login routes
+app.use('/api/user', routes);
+
+//Server path to responses secured routes
+app.use('/api/chat', passport.authenticate('jwt', {
+  session: false
+}), secureRoutes);
+
+
+
+// The 'catchall' handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`API listening on ${port}`);
+
+module.exports = app;
